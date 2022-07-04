c4cf5dbcd077c17f207355c36f1d48db885ef1b8
hvn5 <hvn5@sfu.ca>
2022-07-03 18:52:37 -0700

    Update Onboarding

--- app/src/main/java/com/company/dementiacare/MainActivity.java
+++ app/src/main/java/com/company/dementiacare/MainActivity.java
@@ -1,14 +1,57 @@
 package com.company.dementiacare;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN = 5000;
+
+    //Variables
+    Animation topAnim;
+    Animation botAnim;
+    ImageView image;
+    TextView logo, slogan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //Animations
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        botAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        //Hooks
+        image = findViewById(R.id.imageView);
+        logo = findViewById(R.id.textView1);
+        slogan = findViewById(R.id.textView2);
+
+        image.setAnimation(topAnim);
+        logo.setAnimation(botAnim);
+        slogan.setAnimation(botAnim);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                Intent intent = new Intent(MainActivity.this,Dashboard.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
+
     }
 }
